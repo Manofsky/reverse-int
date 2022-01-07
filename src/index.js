e2cf97220422676904c39337564d6333be7a58d2
Manofsky <clegrof@gmail.com>
2022-01-07 20:05:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(num) {
+  let reversed = 0;
+  while (num) {
+    if (num < 0) {
+      num = Math.abs(num);
+    }
+    reversed = reversed * 10 + num % 10;
+    num = Math.floor(num / 10);
+  }
+  console.log(reversed)
+  return reversed
 }
